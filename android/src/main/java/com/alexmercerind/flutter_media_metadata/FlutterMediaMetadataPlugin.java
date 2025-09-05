01ae2c83e3e34b135bc462563e02ace0a5b5f224
waseemashraf1 <waseemashraf@team.seerox.com>
2025-09-05 17:02:50 +0500

    try release import

--- android/src/main/java/com/alexmercerind/flutter_media_metadata/FlutterMediaMetadataPlugin.java
+++ android/src/main/java/com/alexmercerind/flutter_media_metadata/FlutterMediaMetadataPlugin.java
@@ -15,6 +15,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import java.io.IOException;
 
 public class FlutterMediaMetadataPlugin implements FlutterPlugin, MethodCallHandler {
   private MethodChannel channel;
